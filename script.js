0acc8dccafb6d7964ff62bb9ef3232dbe75e8d91
Muhsinetr <72331917+Muhsinetr@users.noreply.github.com>
2023-09-15 19:42:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 var myGif = document.getElementById("myGif");
+var vav = "myGif/vav.gif"; 
+var bah = "myGif/bah.gif";
+var meem = "myGif/meem.gif"; 
+var fah = "myGif/fah.gif";
+var krah = "myGif/krah.gif"; 
+var goin = "myGif/goin.gif";
+var hah = "myGif/hah.gif"; 
+var ain = "myGif/ain.gif";
+var hhah = "myGif/hhah.gif"; 
+var hamza = "myGif/hamza.gif";
+var lad = "myGif/lad.gif"; 
+var lam = "myGif/lam.gif";
+var kaf = "myGif/kaf.gif"; 
+var qaf = "myGif/qaf.gif";
+var jeem = "myGif/jeem.gif"; 
+var sheen = "myGif/sheen.gif";
+var yah = "myGif/yah.gif"; 
+var twah = "myGif/twah.gif";
+var dal = "myGif/dal.gif"; 
+var thah = "myGif/thah.gif";
+var ssah = "myGif/ssah.gif"; 
+var zal = "myGif/zal.gif";
+var lah = "myGif/lah.gif"; 
+var noon = "myGif/noon.gif";
+var rah = "myGif/rah.gif"; 
+var swad = "myGif/swad.gif";
+var seen = "myGif/seen.gif"; 
+var zah = "myGif/zah.gif";
+
 
 function abc(x) {
   var myAudio = document.getElementById("myAudio");
@@ -6,121 +35,120 @@ function abc(x) {
   switch (x) {
     case 1:
       myAudio.setAttribute("src", "audios/vav.mp3");
-      myGif.setAttribute("src", "myGif/vav.gif");
+      myGif.src = vav;
       break;
     case 2:
       myAudio.setAttribute("src", "audios/bah.mp3");
-      myGif.setAttribute("src", "myGif/bah.gif");
+      myGif.src = bah;
       break; 
     case 3:
       myAudio.setAttribute("src", "audios/meem.mp3");
-      myGif.setAttribute("src", "myGif/meem.gif");
+      myGif.src = meem;
       break;
     case 4:
       myAudio.setAttribute("src", "audios/fah.mp3");
-      myGif.setAttribute("src", "myGif/fah.gif");
+      myGif.src = fah;
       break;
     case 5:
       myAudio.setAttribute("src", "audios/krah.mp3");
-      myGif.setAttribute("src", "myGif/krah.gif");
+      myGif.src = krah;     
       break;
     case 6:
       myAudio.setAttribute("src", "audios/goin.mp3");
-      myGif.setAttribute("src", "myGif/goin.gif");
+      myGif.src = goin;
       break;
     case 7:
       myAudio.setAttribute("src", "audios/hah.mp3");
-      myGif.setAttribute("src", "myGif/hah.gif");
+      myGif.src = hah;
       break;
     case 8:
       myAudio.setAttribute("src", "audios/ain.mp3");
-      myGif.setAttribute("src", "myGif/ain.gif");
+      myGif.src = ain;
       break;
     case 9:
       myAudio.setAttribute("src", "audios/hhah.mp3");
-      myGif.setAttribute("src", "myGif/hhah.gif");
+      myGif.src = hhah;
       break;
     case 10:
       myAudio.setAttribute("src", "audios/hamza.mp3");
-      myGif.setAttribute("src", "myGif/hamza.gif");
+      myGif.src = hamza;
       break;
     case 11:
       myAudio.setAttribute("src", "audios/lad.mp3");
-      myGif.setAttribute("src", "myGif/lad.gif");
+      myGif.src = lad;
       break;
     case 12:
       myAudio.setAttribute("src", "audios/lam.mp3");
-      myGif.setAttribute("src", "myGif/lam.gif");
+      myGif.src = lam ;
       break;
     case 13:
       myAudio.setAttribute("src", "audios/kaf.mp3");
-      myGif.setAttribute("src", "myGif/kaf.gif");
+      myGif.src = kaf;
       break;
     case 14:
       myAudio.setAttribute("src", "audios/qaf.mp3");
-      myGif.setAttribute("src", "myGif/qaf.gif");
+      myGif.src = qaf;
       break;
     case 15:
       myAudio.setAttribute("src", "audios/jeem.mp3");
-      myGif.setAttribute("src", "myGif/jeem.gif");
+      myGif.src = jeem;
       break;
     case 16:
       myAudio.setAttribute("src", "audios/sheen.mp3");
-      myGif.setAttribute("src", "myGif/sheen.gif");
+      myGif.src = sheen;
       break;
     case 17:
       myAudio.setAttribute("src", "audios/yah.mp3");
-      myGif.setAttribute("src", "myGif/yah.gif");
+      myGif.src = yah;
       break;
     case 18:
       myAudio.setAttribute("src", "audios/twah.mp3");
-      myGif.setAttribute("src", "myGif/twah.gif");
+      myGif.src = twah;
       break;
     case 19:
       myAudio.setAttribute("src", "audios/dal.mp3");
-      myGif.setAttribute("src", "myGif/dal.gif");
+      myGif.src = dal;
       break;
     case 20:
       myAudio.setAttribute("src", "audios/thah.mp3");
-      myGif.setAttribute("src", "myGif/thah.gif");
+      myGif.src = thah;
       break;
     case 21:
       myAudio.setAttribute("src", "audios/ssah.mp3");
-      myGif.setAttribute("src", "myGif/ssah.gif");
+      myGif.src = ssah;
       break;
     case 22:
       myAudio.setAttribute("src", "audios/zal.mp3");
-      myGif.setAttribute("src", "myGif/zal.gif");
+      myGif.src = zal;
       break;
     case 23:
       myAudio.setAttribute("src", "audios/lah.mp3");
-      myGif.setAttribute("src", "myGif/lah.gif");
+      myGif.src = lah;
       break;
     case 24:
       myAudio.setAttribute("src", "audios/noon.mp3");
-      myGif.setAttribute("src", "myGif/noon.gif");
+      myGif.src = noon;
       break;
     case 25:
       myAudio.setAttribute("src", "audios/rah.mp3");
-      myGif.setAttribute("src", "myGif/rah.gif");
+      myGif.src = rah;
       break;
     case 26:
       myAudio.setAttribute("src", "audios/swad.mp3");
-      myGif.setAttribute("src", "myGif/swad.gif");
+      myGif.src = swad;
       break;
     case 27:
       myAudio.setAttribute("src", "audios/seen.mp3");
-      myGif.setAttribute("src", "myGif/seen.gif");
+      myGif.src = seen;
       break;
     case 28:
       myAudio.setAttribute("src", "audios/zah.mp3");
-      myGif.setAttribute("src", "myGif/zah.gif");
+      myGif.src = zah;
       break;
   }
   myAudio.load();
   myAudio.play();
 }
-
 var myAudio = document.getElementById("myAudio");
 myAudio.addEventListener('ended', () => {
     myGif.setAttribute("src", "myGif/normal.gif");
