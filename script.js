6c3709eeedc18db531d66fe919876feed94fa778
Mohamed Muhsin <muhsnvemmully1@gmail.com>
2023-09-30 13:23:09 +0530

    audio and picture added

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 
+var harf = ["def","jeem","ssah","thah","bah","hamza","rah","zal","dal","krah","hah","lad","swad","sheen","seen","zah","fah","goin","ain","lah","twah","noon","meem","lam","kaf","qaf","yah","vav","hhah"];
+var myAudio = document.getElementById("myAudio");
+var myGif = document.getElementById("myGif");
 
+function press(x) {
+    var aLink = "audios/"+ harf[x] +".mp3";
+    var gLink = "myGif/"+ harf[x] + ".gif";
 
+    myGif.src = gLink;
+    myAudio.setAttribute("src", aLink);
+    
+    myAudio.load();
+    myAudio.play();
+}
+    
+myAudio.addEventListener('ended', () => {
+    myGif.setAttribute("src", "Files/gifimage.png");
+});
