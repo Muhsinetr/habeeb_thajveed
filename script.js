318ab545fe65e0a7f899422d99f43324710a4ea6
Mohamed Muhsin <muhsnvemmully1@gmail.com>
2023-11-09 10:37:43 +0530

    resposive changes - 2

--- script.js
+++ script.js
@@ -1,43 +1,75 @@
-
-var harf = ["hamza","bah","thah","ssah","jeem","hah","krah","dal","zal","rah","zah","seen","sheen","swad","lad","twah","lah","ain","goin","fah","qaf","kaf","lam","meem","noon","hhah","vav","yah"]
+var harf = [
+  "hamza",
+  "bah",
+  "thah",
+  "ssah",
+  "jeem",
+  "hah",
+  "krah",
+  "dal",
+  "zal",
+  "rah",
+  "zah",
+  "seen",
+  "sheen",
+  "swad",
+  "lad",
+  "twah",
+  "lah",
+  "ain",
+  "goin",
+  "fah",
+  "qaf",
+  "kaf",
+  "lam",
+  "meem",
+  "noon",
+  "hhah",
+  "vav",
+  "yah",
+];
 var myAudio = document.getElementById("myAudio");
 var myGif = document.getElementById("myGif");
 
 function press(x) {
-    var aLink = "audios/"+ harf[x] +".mp3";
-    var gLink = "myGif/"+ harf[x] + ".gif";
+  var aLink = "audios/" + harf[x] + ".mp3";
+  var gLink = "myGif/" + harf[x] + ".gif";
 
-    myGif.src = gLink;
-    myAudio.setAttribute("src", aLink);
-    
-    myAudio.load();
-    myAudio.play();
+  myGif.src = gLink;
+  myAudio.setAttribute("src", aLink);
+
+  myAudio.load();
+  myAudio.play();
 }
-    
-myAudio.addEventListener('ended', () => {
-    myGif.setAttribute("src", "Files/resized.jpg");
+
+myAudio.addEventListener("ended", () => {
+  myGif.setAttribute("src", "Files/resized.jpg");
 });
 
-function menu(){
-    var list = document.getElementById("list");
-    var menu_bar = document.getElementById("menu_icon");
-    if(list.style.display == 'none'){
-    list.style.display = 'block';
+function menu() {
+  var list = document.getElementById("list");
+  var menu_bar = document.getElementById("menu_icon");
+  
+  if (list.style.display == "none") {
+    list.style.display = "block";
     menu_bar.style.backgroundImage = "url('Files/cross.png')";
-    }else{
-        list.style.display = 'none';
-        menu_bar.style.backgroundImage = "url('Files/menu.png')";
-    }
+  } else {
+    list.style.display = "none";
+    menu_bar.style.backgroundImage = "url('Files/menu.png')";
+  }
 }
 
-if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("srworker.js").then(registration=>{
-        console.log("Service worker registered")
-        console.log(registration);
-    }).catch(error=>{
-        console.log("Service worker error")
-        console.log(error)
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("srworker.js")
+    .then((registration) => {
+      console.log("Service worker registered");
+      console.log(registration);
     })
-}else{
-    alert("Service worker not working")
+    .catch((error) => {
+      console.log("Service worker error");
+      console.log(error);
+    });
+} else {
+  alert("Service worker not working");
 }
